6646448e0a84deb0127e3fc1026875683ec8ac78
ataka <masayuki.ataka@gmail.com>
2013-02-09 11:59:01 +0900

    Use MySQL instead of SQLite3.  #1
    $ rails new blog --database=mysql
    
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

--- blog/config/initializers/secret_token.rb
+++ blog/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '2423f3c9939567dfd77795e6ad8c104295205ee91c8c3a90a74a290dced9167125f942663ed011255ce8eb01cfc5bbb4d34bd963fa00ed33ef9c87a270730aa0'
+Blog::Application.config.secret_token = '1ed738a1c3cfc812f61547cad09e8205b1e48d0b4594ef55e6d4e04c60dadccda9ec7421317a1e0f18efa75654715cc21887698a746d07d545bdcf4606a2c579'
