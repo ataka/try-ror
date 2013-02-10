cf0cf52b90254741d0ac38537c6cef175b3f807f
ataka <masayuki.ataka@gmail.com>
2013-02-11 06:07:20 +0900

    Refactoring post_spec.rb.  #8
    :title validation seems wrong.
    It should be fixed.
    
    ref. http://d.hatena.ne.jp/t-wada/20100228/p1
    
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,29 +1,19 @@
-# -*- coding: utf-8 -*-
 require 'spec_helper'
 
-describe Post, "#name が設定されていない場合:" do
+describe Post do
   before do
     @post = Post.new
   end
 
-  it "バリデーションに失敗すること" do
-    @post.should_not be_valid
+  context "#name" do
+    it { should_not be_valid }
+    it { should have(1).errors_on(:name) }
   end
 
-  it ":name にエラーが設定されていること" do
-    @post.should have(1).errors_on(:name)
-  end
-end
+  context "#title" do
+    it { should_not be_valid }
 
-describe Post, "#title が設定されていない場合:" do
-  before do
-    @post = Post.new
+    # requires 'shoulda matchers'
+    # it { should ensure_length_of(:title).is_at_least(5) }
   end
-
-  it "バリデーションに失敗すること" do
-    @post.should_not be_valid
-  end
-
-  # requires 'shoulda matchers'
-  # it { should ensure_length_of(:title).is_at_least(8) }
 end
