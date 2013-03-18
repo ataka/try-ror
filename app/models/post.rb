810105a67ad688b4329b5dd6759f66c90176dfb1
ataka <masayuki.ataka@gmail.com>
2013-03-18 16:02:46 +0900

    Add image per post.  #22
    Copied sample code... It works? don't know...
    
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title, :tags_attributes
+  attr_accessible :content, :name, :title, :tags_attributes, :image
 
   validates :name,  :presence => true
   validates :title, :presence => true,
@@ -7,6 +7,9 @@ class Post < ActiveRecord::Base
 
   has_many  :comments, :dependent => :destroy
   has_many  :tags
+  has_attached_file :image,
+                    :styles => { :medium => "300x300", :thumb => "100x100" },
+                    :default_url => "/images/:style/missing.png"
 
   accepts_nested_attributes_for :tags, :allow_destroy => :true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
