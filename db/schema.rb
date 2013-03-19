c3847b3ee5b7a197a0c02770cd14f33c02a72be4
ataka <masayuki.ataka@gmail.com>
2013-03-19 11:19:05 +0900

    Add db colums for paperclip image files.  #22
    $ rails generate paperclip post image
    $ rake db:migrate
    
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130210170845) do
+ActiveRecord::Schema.define(:version => 20130319020518) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -27,8 +27,12 @@ ActiveRecord::Schema.define(:version => 20130210170845) do
     t.string   "name"
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "tags", :force => true do |t|
