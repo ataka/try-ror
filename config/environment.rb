b50365e2a89f6fec3a5d01b00155c1bc0586bb8d
ataka <masayuki.ataka@gmail.com>
2013-03-21 11:14:58 +0900

    Action Mailer setting for Yahoo! smtp.  #21
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Blog::Application.initialize!
+
+# Action Mailer for Yahoo!
+Rails::Initializer.run do |config|
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.mail.yahoo.com',
+    :port => 587,
+    :authentication => :plain,
+    :user_name => 'username@yahoo.com',
+    :password => 'your_password',
+    :enable_starttls_auto => true
+  }
+end
