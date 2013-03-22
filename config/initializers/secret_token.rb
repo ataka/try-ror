4f371f650640b121e78e3ebff735c654e7558100
ataka <ataka@actindi.net>
2013-03-22 12:23:15 +0900

    git new app,  #19
    with MySQL database, without Unit::Test
    and bundle installed to vendor/bundle (not added).
    
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryRor2::Application.config.secret_token = '06fbd046eb559b077aeb46a754e4139067c5fd53aeaba5fb8406529ff39bbb2325c4a2db3177991e660c0ee2200d261f54c5d2248782793234e85f8d3f63e582'
