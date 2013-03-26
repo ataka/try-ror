de4b978b26dd84c28139cb2834f5946ccb448649
ataka <masayuki.ataka@gmail.com>
2013-03-26 14:14:38 +0900

    Prepare capistrano.
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,36 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'vender/bundler/capistrano'
+load 'deploy/assets'
+
+set :application, "asagao"
+set :repository,  "/var/rails/asagao"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :repository, "."
+set :scm, :none
+set :deploy_via, :copy
+set :copy_exclude, [ "doc", "log", "tmp" ]
+
+set :normalize_asset_timestamps, false
+set :keep_releases, 3
+
+role :web, "asagao.oiax.jp"
+role :app, "asagao.oiax.jp"
+role :db,  "asagao.oiax.jp", :primary => true
+
+after "deploy:update", :roles => :app do
+  run "cp #{shared_path}/config/database.yml #{release_path}/config/"
+end
+
+after "deploy:update", "deploy:cleanup"
+
+namespace :deploy do
+  desc "Restarts your application."
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
